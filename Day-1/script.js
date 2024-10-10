abb1f8f067a2699490d2bb1347fc7f0314d4a5ca
Himanshu Sharma <sharmaslov@gmail.com>
2024-10-10 16:21:22 +0530

    day-2

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
 const material = new THREE.MeshBasicMaterial( { color: 0xff5e00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
